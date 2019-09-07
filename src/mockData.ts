e000feb8a33602bd7e0bc712938275f87b54bc9f
Patrik Vácal <patrik.vacal@gmail.com>
2019-09-07 16:51:05 +0200

    Added test for GET /calendar/going

--- src/mockData.ts
+++ src/mockData.ts
@@ -3,6 +3,28 @@ export const MOCK_PARENT_PASSWORD = 'parent_passw0rd!';
 export const MOCK_PARENT_FULLNAME = 'parent_kadibudka';
 export const MOCK_PARENT_EMAIL = 'parent_kadibudkas@email.cz';
 export const MOCK_PARENT_KIDS = ['Matěj Obecný', 'Eliška'];
+
+function generateDays(pseudoSeed: number) {
+    const days = [];
+    for (let i = 0; i <= 30; i++) {
+        days.push({
+            date: `2019-09-${i.toString().padStart(2, '0')}`,
+            value: (i % pseudoSeed === 0),
+        });
+    }
+    return days;
+}
+
+export const TEST_PARENT_KIDS_CALENDAR_2019_09 = [
+    {
+        fullname: 'Matěj Obecný',
+        days: generateDays(3),
+    },
+    {
+        fullname: 'Eliška',
+        days: generateDays(4),
+    },
+];
 export const MOCK_PARENT_NONEXISTING_USERNAME = 'parent_kadibudka';
 export const MOCK_PARENT_NONEXISTING_PASSWORD = 'parent_passw0rd!';
 export const MOCK_CHANGE_NEW_PASSWORD = 'parent_new_passw0rd!';
---
+++ test/getCalendarGoing.test.ts
@@ -0,0 +1,22 @@
+import * as request from 'supertest';
+import { SuperTest, Test } from 'supertest';
+import { app } from '../src/app';
+import { createAuthenticatedRequest } from './testUtils/createAuthenticatedRequest';
+import { TEST_PARENT_KIDS_CALENDAR_2019_09 } from '../src/mockData';
+
+export default describe('getUserInfo route', () => {
+    it('should not get user calendar because of unauthorized', () =>
+        request(app)
+            .get(`/calendar/going`)
+            .expect(401));
+
+    it('should get user calendar going info', () =>
+        createAuthenticatedRequest((req: SuperTest<Test>) => {
+            req
+                .get(`/calendar/going`)
+                .expect(200)
+                .expect({
+                    kids: TEST_PARENT_KIDS_CALENDAR_2019_09,
+                });
+        }));
+});
